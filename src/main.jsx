9fc633a7ac8d7f1d406b61037fbcced1e603ef9e
Debojeet Karmakar <debojeetkarmakar2020@gmail.com>
2024-05-23 19:42:22 +0530

    chnaged routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Container } from './Components/compnents'
 
 
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import {ErrorPage, Home, Each_Projects,Project, Experiences, Each_Experience} from './Pages/routes.js'
 
 
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/project",
-        element: <Project />,
-      },
-      {
-        path: "/project/:projectname",
-        element: <Each_Projects />,
-      },
-      {
-        path: "/experiences",
-        element: <Experiences />,
-      },
-      {
-        path: "/experiences/:experiencename",
-        element: <Each_Experience />,
-      },
-    ],
+    element: <Home />,
   },
-  
+  {
+    path: "/project",
+    element: <Project />,
+  },
+  {
+    path: "/project/:projectname",
+    element: <Each_Projects />,
+  },
+  {
+    path: "/experiences",
+    element: <Experiences />,
+  },
+  {
+    path: "/experiences/:experiencename",
+    element: <Each_Experience />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage/>,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router}/>  
+    <Container classes={`px-5 flex flex-col items-center justify-start cursor-default selection:bg-stone-500 `}>
+      <RouterProvider router={router}/> 
+    </Container>
   </React.StrictMode>,
 )
