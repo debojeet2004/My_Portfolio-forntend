d2f7b90cdbcf45742c3c1b20dcfa1a8375557f59
Debojeet Karmakar <debojeetkarmakar2020@gmail.com>
2024-05-23 22:41:15 +0530

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='border w-full p-5 flex justify-center items-center'>
+    <div className='w-full p-5 flex justify-center items-center'>
       <p className='text-center'>Made with lOvE and 💻 by Debojeet Karmakar :)</p>
     </div>
   )
